20903f1db5a6a6370d09c5e8b8015967f5b84619
Tina Razic <tina.razic@gmail.com>
2018-10-14 22:54:33 +0200

    poskus 2.naloge

--- Vaja_1.R
+++ Vaja_1.R
@@ -2,8 +2,10 @@
 # obdobje 2011 - 2013, 3 x 6 
 # 1. Uvoz podatkov in dinamika obrestnih mer v ?asu
 #a)uvoz
-
+library(dplyr)
 library(readr)
+library(data.table)
+
 hist_EURIBOR_2011 <- read_csv("hist_EURIBOR_2011.csv")
 hist_EURIBOR_2012 <- read_csv("hist_EURIBOR_2012.csv")
 hist_EURIBOR_2013 <- read_csv("hist_EURIBOR_2013.csv")
@@ -29,18 +31,37 @@ obresti_2012 <- obresti_2012[-1, ]
 colnames(obresti_2013) <- obresti_2013[1, ]
 obresti_2013 <- obresti_2013[-1, ]
 
-# - združimo tabele v eno
+# - zdru?imo tabele v eno
 obresti <- rbind(obresti_2011,obresti_2012,obresti_2013)
 
 #c) narišemo graf
-#naredimo 2 časovni vrsti, T1 za 6 mesecev, U1 za 12 mesecev
+#naredimo 2 ?asovni vrsti, T1 za 6 mesecev, U1 za 12 mesecev
 
 T1 <- ts(data = obresti[,9],start = c(2011,1),frequency = 12)
 U1 <- ts(data = obresti[,15],start = c(2011,1),frequency = 12)
 
-#narišemo graf
+#nari?emo graf
 require(graphics)
 
-ts.plot(T1,U1,xlab = "Time",ylab = "%", main = "EURIBOR", col = c("darksalmon","cornflowerblue"))
+grafEURIBOR <- ts.plot(T1,U1,xlab = "Time",ylab = "%", main = "EURIBOR", col = c("darksalmon","cornflowerblue"))
 legend("topright",legend=c("6 mesecev", "12 mesecev"),col=c("darksalmon", "cornflowerblue"),lty=1)
 
+
+#2. Oblika obrestne krivulje 
+#a) izbira datumov:01/04/2011 razmik & 01/08/2012 hrib?ek1 & 01/02/2013 hrib?ek2
+#b)
+
+library(ggplot2)
+
+obrestiD <- data.table(t(obresti[c(4,20,26),]))
+
+grafD <- ggplot(obrestiD,aes(x = obrestiD[,0],y = obrestiD[1,])) + geom_line(aes(colour=variable))
+
+
+
+#3. Hipoteza pri?akovanj trga
+#a)
+izracun_terminske <- function(T,U,obrestT,obrestU){
+  terminska = (1/(U-T))*(((1+U*obrestU)/(1+T*obrestT))-1)
+  return (terminska)
+}
